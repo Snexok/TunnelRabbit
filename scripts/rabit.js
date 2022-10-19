7086baf02ce25ca5f9e2d9e3a093d4dfb24393e2
Snexok <30509691+Snexok@users.noreply.github.com>
2022-10-19 20:31:19 +0600

    Pre completed

--- scripts/rabit.js
+++ scripts/rabit.js
@@ -6,33 +6,44 @@ let w = canvas.width = 1200
 
 let textColor = 'wheat'
 
-let rabbit = 0
-let tunnel = 0
-let tunnelStrocke = 0
-let tunnelEntrance = 0
-let tunnelEntranceStrocke = 0
-let tunnelExit = 0
-let tunnelExitStrocke = 0
-let openTunnel = 0
-let line = 0
-
+let soundHit = {}
 let images = {}
+
+// Game objects
+let rabbit = {}
+let tunnel = {}
+let tunnelEntrance = {}
+let tunnelExit = {}
+let openTunnel = {}
+let trap = {}
+let line = {}
 
 let showRabbit = false
 
-// running gameover
-let gameStatus = "running"
-let score = 0
+// preIntro intro running gameover
+let gameStatus = "preIntro"
+let gameOverText = "You "
 
 window.addEventListener("keyup", (e) => {
     if (String(e.keyCode)==='32'){
-        if(gameStatus === "gameOver") {
-            document.location.reload()
-        }
-        else {
-            if (rabbit.status==="inTunnel")
-                if (Math.cos(rabbit.angle) > 0.68 && rabbit.x<tunnel.x)
-                    score+=1
+        switch (gameStatus) {
+            case "inTunnel":
+                if (rabbit.angle/Math.PI%2 > openTunnel.startAngle/Math.PI+0.5 && rabbit.angle/Math.PI%2 < openTunnel.endAngle/Math.PI+0.5)
+                    gameOverText += "Win"
+                else
+                    gameOverText += "Lose"
+
+                if (rabbit.angle/Math.PI%2 > trap.startAngle/Math.PI+0.5 && rabbit.angle/Math.PI%2 < trap.endAngle/Math.PI+0.5)
+                    soundHit.play();
+
+                gameStatus = "gameOver"
+                break
+            case "preIntro":
+                gameStatus = "intro"
+                break
+            case "gameOver":
+                document.location.reload()
+                break
 
         }
     }
@@ -42,39 +53,55 @@ window.addEventListener("keyup", (e) => {
     else if (String(e.keyCode)==='82') {
         document.location.reload()
     }
+    else if (String(e.keyCode)==='189') {
+        if (gameStatus === "preIntro") {
+            if (rabbit.speed > 1) {
+                rabbit.speed -= 1
+                rabbit.x -= rabbit.speed + rabbit.x % rabbit.speed - tunnel.x % rabbit.speed
+            }
+        }
+    }
+    else if (String(e.keyCode)==='187') {
+        if (gameStatus === "preIntro") {
+            if (rabbit.speed < 5) {
+                rabbit.speed += 1
+                rabbit.x += rabbit.speed - rabbit.x % rabbit.speed + tunnel.x % rabbit.speed
+            }
+        }
+    }
 })
 
-class Circle {
-    constructor(x, y, color, radius=20, lineWidth=1, speed=0) {
-        this.speed = speed
-        this.x = x
-        this.y = y
-        this.color = color
-        this.radius = radius
-        this.angle = 0
-        this.lineWidth = lineWidth
-    }
-}
-
-class Line {
-    constructor(startX=0, endX=555, startY=182, endY=182, color) {
+class Line{
+    constructor(startX=20, endX=555, startY=182, endY=182, color='black', lineWidth=5) {
         this.startX = startX
         this.endX = endX
         this.startY = startY
         this.endY = endY
         this.color = color
-        this.angle = 0
+        this.lineWidth = lineWidth
     }
+
+    update(ctx) {
+        if (this.startX===this.endX)
+            return
+
+        if (gameStatus === "inTunnel")
+            if (this.startX<this.endX) {
+                this.startX += 35
+                this.draw(ctx)
+            }
+            else
+                this.startX=this.endX
+        else
+            this.draw(ctx)
+    }
+
     draw(ctx) {
         // set line stroke and line width
-        ctx.strokeStyle = 'black';
-        ctx.lineWidth = 5;
+        ctx.strokeStyle = this.color;
+        ctx.lineWidth = this.lineWidth;
 
-        if (rabbit.status==="inTunnel")
-            if (this.startX<=this.endX)
-                this.startX+=35
-
-        // draw a red line
+        // draw a line
         ctx.beginPath();
         ctx.moveTo(this.startX, this.startY);
         ctx.lineTo(this.endX, this.endY);
@@ -82,45 +109,102 @@ class Line {
     }
 }
 
+class Obj {
+    constructor(x, y, color, lineWidth=1) {
+        this.x = x
+        this.y = y
+        this.color = color
+        this.lineWidth = lineWidth
+    }
+}
+
+class Circle extends Obj{
+    constructor(x, y, color, radius=20, lineWidth=1) {
+        super(x, y, color, lineWidth)
+        this.radius = radius
+    }
+}
+
+
 class Tunnel extends Circle {
-    constructor(x, y, startAngle, endAngel, color, radius = 20, lineWidth = 1, speed = 0) {
-        super(x, y, color, radius, lineWidth, speed)
+    constructor(x, y, startAngle, endAngle, color, radius = 20, lineWidth = 1, strokeWidth = 0, strokeColor="black") {
+        super(x, y, color, radius, lineWidth)
         this.startAngle = startAngle
-        this.endAngel = endAngel
+        this.endAngle = endAngle
+        this.strokeWidth = strokeWidth
+        this.strokeColor = strokeColor
     }
 
     draw(ctx) {
         let pattern = 0
-        console.log(this.color)
         if (this.color==='tunnel')
             pattern = ctx.createPattern(images['brick'], "repeat");
         else if (this.color==='tunnelEntrance')
             pattern = ctx.createPattern(images['tunnelEntrance'], "repeat");
+
+
+        // Strocke
         ctx.beginPath();
-        if (this.color==='tunnel' || this.color==='tunnelEntrance')
+        if (this.color !== "openTunnel") {
+            ctx.arc(this.x, this.y, this.radius, this.startAngle - this.strokeWidth / 100 / 4, this.endAngle + this.strokeWidth / 100 / 4);
+            ctx.strokeStyle = this.strokeColor;
+            ctx.lineWidth = this.lineWidth + this.strokeWidth
+            ctx.stroke()
+        }
+
+        // Tunnel
+        ctx.beginPath();
+        if (this.color === 'tunnel' || this.color === 'tunnelEntrance')
             ctx.strokeStyle = pattern;
-        else
-            ctx.strokeStyle = this.color;
-        ctx.arc(this.x, this.y, this.radius, this.startAngle, this.endAngel);
+        else if (this.color === 'openTunnel')
+            ctx.strokeStyle = 'limegreen'
+        ctx.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle);
         ctx.lineWidth = this.lineWidth
         ctx.stroke()
     }
 }
 
-class Rabbit extends Circle {
+class Trap extends Circle {
+    constructor(x, y, startAngle, endAngle, color, radius = 20, lineWidth = 1, strokeWidth = 0, strokeColor="black") {
+        super(x, y, color, radius, lineWidth)
+        this.startAngle = startAngle
+        this.endAngle = endAngle
+        this.strokeWidth = strokeWidth
+        this.strokeColor = strokeColor
+    }
+
+    draw(ctx) {
+        if (showRabbit || gameStatus==="gameOver") {
+            ctx.beginPath();
+            let pattern = ctx.createPattern(images['jail'], "repeat");
+            ctx.strokeStyle = pattern
+            ctx.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle);
+            ctx.lineWidth = this.lineWidth
+            ctx.stroke()
+        }
+    }
+}
+
+
+class Rabbit extends Obj {
     constructor(x, y, color, radius=20, lineWidth=1, speed=0) {
         // we join the entrance to the tunnel and the speed of the movement of the rabbit
         x += speed - x % speed + tunnel.x % speed
 
         super(x, y, color, radius, lineWidth, speed)
-        this.status = "intro"
+        this.speed = speed
         this.time = 0
         this.timeFrime = 0
+        this.angle = 0
     }
 
     move() {
         this.check(tunnel)
-        switch (this.status) {
+        switch (gameStatus) {
+            case "preIntro":
+                break
+            case "gameOver":
+                break
             case "intro":
                 this.x += Math.cos(this.angle) * this.speed
                 this.y += Math.sin(this.angle) * this.speed
@@ -140,11 +224,11 @@ class Rabbit extends Circle {
         if(this.y + this.radius > h || this.y - this.radius < 0) this.angle += Math.PI
 
         if (this.x===tunnel.x)
-            this.status = "inTunnel"
+            gameStatus = "inTunnel"
     }
 
     draw(ctx, currentFrame) {
-        if (this.status==="inTunnel") {
+        if (gameStatus === "inTunnel") {
             if (Math.cos(this.angle) < 0.74 || this.x > tunnel.x) {
                 if(!showRabbit)
                     return
@@ -205,6 +289,9 @@ loadImages = function() {
     },{
         name: 'tunnelEntrance',
         id: 'tunnelEntrance'
+    },{
+        name: 'jail',
+        id: 'jail'
     }]
     let numImages = imageSources.length;
     for (let i = numImages - 1; i >= 0; i--) {
@@ -213,55 +300,69 @@ loadImages = function() {
     }
 }
 
+
+function decodeBase64ToArrayBuffer(base64String) {
+    let len = (base64String.length / 4) * 3;
+    let str = atob(base64String);
+    let arrayBuffer = new ArrayBuffer(len);
+    let bytes = new Uint8Array(arrayBuffer);
+
+    for (let i = 0; i < len; i++) {
+        bytes[i] = str.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+
+loadSounds = function() {
+    soundHit = new Audio('assets/offline-sound-hit.mp3');
+    soundHit.volume = 1
+}
+
+
 function setUp() {
-    gameStatus = "running"
-    score = 0
     loadImages()
-    tunnel = new Tunnel(550, 350,29.85, Math.PI * 3+0.8, "tunnel",  200, 60)
-    tunnelStrocke = new Tunnel(550, 350,29.83, Math.PI * 3+0.80, "black",  200, 68)
-    tunnelExit = new Tunnel(550, 350,Math.PI+0.7, Math.PI+0.8, "tunnelEntrance",  200, 80)
-    tunnelExitStrocke = new Tunnel(550, 350,Math.PI+0.68, Math.PI+0.82, "black",  200, 90)
-    tunnelEntrance = new Tunnel(550, 350,4.68, 4.78, "tunnelEntrance",  200, 80)
-    tunnelEntranceStrocke = new Tunnel(550, 350,4.66, 4.8, "black",  200, 90)
-    openTunnel = new Tunnel(550, 350,Math.PI+0.8, 4.74, "limegreen",  182, 26)
-    rabbit = new Rabbit(20, 150, "blue",  20, 1, 2)
+    loadSounds()
+    trap = new Trap(550, 350,Math.PI-1, Math.PI - 0.1, "jail",  200, 60, 8, "black")
+    tunnel = new Tunnel(550, 350,Math.PI * 2 - 1.57, Math.PI + 0.8, "tunnel",  200, 60, 8, "black")
+    tunnelExit = new Tunnel(550, 350,Math.PI + 0.7, Math.PI + 0.8, "tunnelEntrance",  200, 80, 8, "black")
+    tunnelEntrance = new Tunnel(550, 350,Math.PI + 1.53, Math.PI + 1.63, "tunnelEntrance",  200, 80, 8, "black")
+    openTunnel = new Tunnel(550, 350,Math.PI + 0.8, Math.PI + 1.63, "openTunnel",  182, 26)
+    rabbit = new Rabbit(30, 150, "blue",  20, 1, 2)
     line = new Line()
 }
 
 function gameLoop() {
     ctx.clearRect(0, 0, w, h)
-    if(gameStatus === "gameOver") {
-        ctx.font = "30px Comic"
-        ctx.fillText("Game Over", w/2 - 150, h/2 - 100)
-        ctx.fillText("you have scored : " + score, w/2 - 150, h/2)
-        return
-    }
 
+    ctx.fillStyle = textColor
+
+    ctx.font = "24px Comic"
+    ctx.fillText("Rabbit speed: " + rabbit.speed , 20, 24)
 
     ctx.font = "14px Comic"
-    ctx.fillStyle = textColor
-    ctx.fillText("Press SPACE when the circle came out from tunnel" , 20, 250)
-    ctx.fillText("to score points" , 20, 264)
-    ctx.fillText("Press H to hide a circle" , 20, 284)
-    ctx.fillText("Press R to restart the game" , 20, 304)
+    ctx.fillText("Press SPACE to perform the main action" , 20, 250+20*0)
+    ctx.fillText("Press - to down the rabbit's speed" , 20, 250+20*1)
+    ctx.fillText("Press + to up the rabbit's speed" , 20, 250+20*2)
+    ctx.fillText("Press H to hide a rabbit" , 20, 250+20*3)
+    ctx.fillText("Press R to restart the game" , 20, 250+20*4)
 
     openTunnel.draw(ctx)
-    tunnelStrocke.draw(ctx)
     tunnel.draw(ctx)
-    line.draw(ctx)
+
+    line.update(ctx)
 
     rabbit.update(ctx)
 
-    tunnelEntranceStrocke.draw(ctx)
+
+    trap.draw(ctx)
     tunnelEntrance.draw(ctx)
-    tunnelExitStrocke.draw(ctx)
     tunnelExit.draw(ctx)
 
+    if(gameStatus === "gameOver") {
+        ctx.font = "30px Comic"
+        ctx.fillText(gameOverText, w/2 -110, h/2 + 60)
+    }
     requestAnimationFrame(gameLoop)
-}
-
-function random(to, from = 0) {
-    return Math.floor(Math.random() * (to - from) + from)
 }
 
 
