782331ab1aeea47e1d770b7a15e5e0455fac1173
Snexok <30509691+Snexok@users.noreply.github.com>
2022-11-08 23:40:16 +0400

    правки #1
    1. Заяц должен двигаться плавно
    2. (Лужайка) Тоннель должен быть «съемный»
    3. (Ловушка поверх туннеля) На тоннеле (окружности) должна быть видна зона «риска»
    4. На тоннеле (окружности) должна быть возможность обозначить выбранную точку остановки ()
    5. Громкость звука наказания должна коррелироваться со скоростью движения - чем выше скорость, тем громче звук наказания
    6. Должна быть возможность убрать «зону риска» (она же ловушка), для тренировки точности и скорости остановки.
    7. Сохранение результатов сессии

--- scripts/rabit.js
+++ scripts/rabit.js
@@ -17,43 +17,89 @@ let tunnelExit = {}
 let openTunnel = {}
 let trap = {}
 let line = {}
+let targetPoint = {}
 
-let showRabbit = false
+// Session control
+let sessionRes = []
+let res = "" // "Попался в ловушку", "Попал в цель", "Промахнулся"
+
+// let showRabbit = false
+let showTunnel = true
 
 // preIntro intro running gameover
 let gameStatus = "preIntro"
-let gameOverText = "You "
+// let gameOverText = "You "
+let gameOverText = "Game Over"
+
+
+window.addEventListener("mousedown", (e) => {
+    let duration = Math.sqrt((e.x-tunnel.x)**2+(e.y-tunnel.y)**2)
+    if (duration >= tunnel.radius-tunnel.lineWidth/2 && duration <= tunnel.radius+tunnel.lineWidth/2) {
+        duration = Math.sqrt((e.x-targetPoint.x)**2+(e.y-targetPoint.y)**2)
+        if (duration <= targetPoint.radius) {
+            targetPoint.x = 0
+            targetPoint.y = 0
+            targetPoint.seted = false
+        }
+        else{
+            targetPoint.angle = Math.atan2(e.y - tunnel.y, e.x - tunnel.x)
+            targetPoint.x = tunnel.radius * Math.cos(targetPoint.angle) + tunnel.x
+            targetPoint.y = tunnel.radius * Math.sin(targetPoint.angle) + tunnel.y
+            targetPoint.seted = true
+        }
+    }
+})
 
 window.addEventListener("keyup", (e) => {
-    if (String(e.keyCode)==='32'){
+    if (String(e.keyCode)==='32'){ // пробел|space
         switch (gameStatus) {
             case "inTunnel":
-                if (rabbit.angle/Math.PI%2 > openTunnel.startAngle/Math.PI+0.5 && rabbit.angle/Math.PI%2 < openTunnel.endAngle/Math.PI+0.5)
-                    gameOverText += "Win"
+                // if (rabbit.angle/Math.PI%2 > openTunnel.startAngle/Math.PI+0.5 && rabbit.angle/Math.PI%2 < openTunnel.endAngle/Math.PI+0.5)
+                //     gameOverText += "Win"
+                // else
+                //     gameOverText += "Lose"
+
+                if (rabbit.angle/Math.PI%2 > trap.startAngle/Math.PI+0.5 && rabbit.angle/Math.PI%2 < trap.endAngle/Math.PI+0.5) {
+                    res = "Попался в ловушку"
+                    soundHit.volume = 0.2 * rabbit.speed
+                    soundHit.play()
+                }
                 else
-                    gameOverText += "Lose"
+                    if (rabbit.angle/Math.PI%2 > targetPoint.angle/Math.PI+0.5-targetPoint.radius/1000 &&
+                    rabbit.angle/Math.PI%2 < targetPoint.angle/Math.PI+0.5+targetPoint.radius/1000) {
 
-                if (rabbit.angle/Math.PI%2 > trap.startAngle/Math.PI+0.5 && rabbit.angle/Math.PI%2 < trap.endAngle/Math.PI+0.5)
-                    soundHit.play();
+                    res = "Попал в цель"
+                }
+                else {
+                    res = "Промахнулся"
+                }
 
+                sessionRes.push(res)
                 gameStatus = "gameOver"
                 break
             case "preIntro":
                 gameStatus = "intro"
                 break
             case "gameOver":
-                document.location.reload()
+                setUp(true)
                 break
 
         }
     }
-    else if (String(e.keyCode)==='72') {
-        showRabbit = showRabbit ? false : true
+    else if (String(e.keyCode)==='69') { // e|у
+        // showRabbit = !showRabbit
+        showTunnel = !showTunnel
     }
-    else if (String(e.keyCode)==='82') {
-        document.location.reload()
+    else if (String(e.keyCode)==='84') { // t|е
+        trap.seted = !trap.seted
     }
-    else if (String(e.keyCode)==='189') {
+    else if (String(e.keyCode)==='82') { // r|к
+        setUp(true)
+    }
+    else if (String(e.keyCode)==='89') { // y|н
+        saveSession()
+    }
+    else if (String(e.keyCode)==='189') { // -
         if (gameStatus === "preIntro") {
             if (rabbit.speed > 1) {
                 rabbit.speed -= 1
@@ -61,7 +107,7 @@ window.addEventListener("keyup", (e) => {
             }
         }
     }
-    else if (String(e.keyCode)==='187') {
+    else if (String(e.keyCode)==='187') { // +
         if (gameStatus === "preIntro") {
             if (rabbit.speed < 5) {
                 rabbit.speed += 1
@@ -69,10 +115,25 @@ window.addEventListener("keyup", (e) => {
             }
         }
     }
+    else if (+String(e.keyCode)<=53 && +String(e.keyCode)>=49) { // 1-2-3-4-5
+        if (gameStatus === "preIntro") {
+            let newSpeed = +String(e.keyCode) - 48
+            if (rabbit.speed > newSpeed)
+                for (let i = rabbit.speed; i > newSpeed; i--) {
+                    rabbit.speed -= 1
+                    rabbit.x -= rabbit.speed + rabbit.x % rabbit.speed - tunnel.x % rabbit.speed
+                }
+            else if (rabbit.speed < newSpeed)
+                for (let i = rabbit.speed; i < newSpeed; i++) {
+                    rabbit.speed += 1
+                    rabbit.x += rabbit.speed - rabbit.x % rabbit.speed + tunnel.x % rabbit.speed
+                }
+        }
+    }
 })
 
 class Line{
-    constructor(startX=20, endX=555, startY=182, endY=182, color='black', lineWidth=5) {
+    constructor(startX=20, endX=555, startY=178, endY=178, color='limegreen', lineWidth=5) {
         this.startX = startX
         this.endX = endX
         this.startY = startY
@@ -144,23 +205,27 @@ class Tunnel extends Circle {
 
 
         // Strocke
-        ctx.beginPath();
-        if (this.color !== "openTunnel") {
-            ctx.arc(this.x, this.y, this.radius, this.startAngle - this.strokeWidth / 100 / 4, this.endAngle + this.strokeWidth / 100 / 4);
-            ctx.strokeStyle = this.strokeColor;
-            ctx.lineWidth = this.lineWidth + this.strokeWidth
-            ctx.stroke()
+        if (showTunnel) {
+            ctx.beginPath();
+            if (this.color !== "openTunnel") {
+                ctx.arc(this.x, this.y, this.radius, this.startAngle - this.strokeWidth / 100 / 4, this.endAngle + this.strokeWidth / 100 / 4);
+                ctx.strokeStyle = this.strokeColor;
+                ctx.lineWidth = this.lineWidth + this.strokeWidth
+                ctx.stroke()
+            }
         }
 
         // Tunnel
         ctx.beginPath();
-        if (this.color === 'tunnel' || this.color === 'tunnelEntrance')
+        if (showTunnel && (this.color === 'tunnel' || this.color === 'tunnelEntrance'))
             ctx.strokeStyle = pattern;
         else if (this.color === 'openTunnel')
             ctx.strokeStyle = 'limegreen'
-        ctx.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle);
-        ctx.lineWidth = this.lineWidth
-        ctx.stroke()
+        if (showTunnel || this.color === 'openTunnel') {
+            ctx.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle);
+            ctx.lineWidth = this.lineWidth
+            ctx.stroke()
+        }
     }
 }
 
@@ -171,17 +236,17 @@ class Trap extends Circle {
         this.endAngle = endAngle
         this.strokeWidth = strokeWidth
         this.strokeColor = strokeColor
+        this.seted = true
     }
 
     draw(ctx) {
-        if (showRabbit || gameStatus==="gameOver") {
-            ctx.beginPath();
-            let pattern = ctx.createPattern(images['jail'], "repeat");
-            ctx.strokeStyle = pattern
-            ctx.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle);
-            ctx.lineWidth = this.lineWidth
-            ctx.stroke()
-        }
+        ctx.beginPath();
+        let pattern = ctx.createPattern(images['jail'], "repeat");
+        ctx.strokeStyle = pattern
+        ctx.lineWidth = showTunnel ? tunnel.lineWidth : rabbit.height
+        this.radius = showTunnel ? tunnel.radius : openTunnel.radius
+        ctx.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle);
+        ctx.stroke()
     }
 }
 
@@ -196,6 +261,8 @@ class Rabbit extends Obj {
         this.time = 0
         this.timeFrime = 0
         this.angle = 0
+        this.height = 60
+        this.width = 22
     }
 
     move() {
@@ -228,12 +295,12 @@ class Rabbit extends Obj {
     }
 
     draw(ctx, currentFrame) {
-        if (gameStatus === "inTunnel") {
-            if (Math.cos(this.angle) < 0.74 || this.x > tunnel.x) {
-                if(!showRabbit)
-                    return
-            }
-        }
+        // if (gameStatus === "inTunnel") {
+        //     if (Math.cos(this.angle) < 0.74 || this.x > tunnel.x) {
+        //         if(!showRabbit)
+        //             return
+        //     }
+        // }
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angle)
@@ -241,9 +308,9 @@ class Rabbit extends Obj {
 
         ctx.drawImage(images["rabbit"],
             currentFrame.x, currentFrame.y,
-            22, 60,
-            this.x-11, this.y-30,
-            22, 60)
+            this.width, this.height,
+            this.x-this.width/2, this.y-this.height/2,
+            this.width, this.height)
         ctx.restore();
     }
 
@@ -255,27 +322,83 @@ class Rabbit extends Obj {
         this.move()
 
         let currentFrame = {x:0, y:0}
-        if (this.timeFrime>100) {
-            currentFrame.x += 22
-        }
-        if (this.timeFrime>200) {
-            currentFrame.x += 22
-        }
-        if (this.timeFrime>300) {
-            currentFrame.x += 22
-        }
-        if (this.timeFrime>400) {
-            currentFrame.x += 22
-        }
-        if (this.timeFrime>500) {
-            currentFrame.x += 22
-        }
-        if (this.timeFrime>600) {
-            this.timeFrime = 0
-        }
-        this.draw(ctx, currentFrame)
+
+     if (gameStatus !== "preIntro" && gameStatus !== "gameOver") {
+         if (this.timeFrime > 100) {
+             currentFrame.x += 22
+         }
+         if (this.timeFrime > 200) {
+             currentFrame.x += 22
+         }
+         if (this.timeFrime > 300) {
+             currentFrame.x += 22
+         }
+         if (this.timeFrime > 400) {
+             currentFrame.x += 22
+         }
+         if (this.timeFrime > 500) {
+             currentFrame.x += 22
+         }
+         if (this.timeFrime > 600) {
+             this.timeFrime = 0
+         }
+     }
+    this.draw(ctx, currentFrame)
 
     }
+}
+
+
+class TargetPoint extends Obj {
+    constructor(x=0, y=0, color="green", radius = 30, lineWidth = 1) {
+        super(x, y, color, radius, lineWidth)
+        this.angle = 0
+        this.radius = radius
+        this.seted = false
+    }
+
+    draw(ctx) {
+        if (this.seted) {
+            ctx.beginPath();
+            ctx.fillStyle = this.color;
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+            ctx.fill();
+            ctx.beginPath();
+            ctx.fillStyle = "red";
+            ctx.arc(this.x, this.y, this.radius/2, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    }
+}
+
+function download(data, filename, type) {
+    let file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        let a = document.createElement("a"),
+            url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        }, 0);
+    }
+}
+
+function saveSession() {
+    console.log(sessionRes)
+    let resText = ""
+
+    for (let i=0; i < sessionRes.length; i++) {
+        console.log(sessionRes[i])
+        resText += "Попытка номер " + (i+1) + ": " + sessionRes[i] + "\n"
+    }
+    download(resText, "Результат сессии "+(Math.random() + 1).toString(36).substring(7), 'text/plain')
+    sessionRes = []
 }
 
 
@@ -307,46 +430,67 @@ loadSounds = function() {
 }
 
 
-function setUp() {
-    loadImages()
-    loadSounds()
+function setUp(restart=false) {
+    if (!restart) {
+        loadImages()
+        loadSounds()
+    }
+    showTunnel = true
+    gameStatus = "preIntro"
+
+    sessionRes = []
+
     trap = new Trap(550, 350,Math.PI-1, Math.PI - 0.1, "jail",  200, 60, 8, "black")
     tunnel = new Tunnel(550, 350,Math.PI * 2 - 1.57, Math.PI + 0.8, "tunnel",  200, 60, 8, "black")
     tunnelExit = new Tunnel(550, 350,Math.PI + 0.7, Math.PI + 0.8, "tunnelEntrance",  200, 80, 8, "black")
     tunnelEntrance = new Tunnel(550, 350,Math.PI + 1.53, Math.PI + 1.63, "tunnelEntrance",  200, 80, 8, "black")
-    openTunnel = new Tunnel(550, 350,Math.PI + 0.8, Math.PI + 1.63, "openTunnel",  182, 26)
+    openTunnel = new Tunnel(550, 350,0, Math.PI *2, "openTunnel",  182, 26)
     rabbit = new Rabbit(30, 150, "blue",  20, 1, 2)
     line = new Line()
+    targetPoint = new TargetPoint()
 }
+
 
 function gameLoop() {
     ctx.clearRect(0, 0, w, h)
 
     ctx.fillStyle = textColor
+    ctx.font = "24px Helvetica"
+    ctx.fillText("Скорость: " + rabbit.speed , 20, 24)
 
-    ctx.font = "24px Comic"
-    ctx.fillText("Rabbit speed: " + rabbit.speed , 20, 24)
+    ctx.fillStyle = trap.seted ? "#c00" : "limegreen"
+    ctx.font = "24px Helvetica"
+    ctx.fillText("Ловушка " + (trap.seted ? "активирована" : "отсутствует") , 180, 24)
 
-    ctx.font = "14px Comic"
-    ctx.fillText("Press SPACE to perform the main action" , 20, 250+20*0)
-    ctx.fillText("Press - to down the rabbit's speed" , 20, 250+20*1)
-    ctx.fillText("Press + to up the rabbit's speed" , 20, 250+20*2)
-    ctx.fillText("Press H to hide a rabbit" , 20, 250+20*3)
-    ctx.fillText("Press R to restart the game" , 20, 250+20*4)
+    ctx.fillStyle = textColor
+    ctx.font = "16px Helvetica"
+    ctx.fillText("Пробел -> действие", 20, 450+20*0)
+    ctx.fillText("1-2-3-4-5 -> скорость бега", 20, 450+20*1)
+    ctx.fillText("У -> убрать туннель", 20, 450+20*2)
+    ctx.fillText("К -> начать испытание заново", 20, 450+20*3)
+    ctx.fillText("Е -> " + (trap.seted ? "убрать" : "активировать") + " ловушку", 20, 450+20*4)
+    ctx.fillText("Н -> сохранить сессию и начать новую", 20, 450+20*5)
 
     openTunnel.draw(ctx)
-    tunnel.draw(ctx)
 
     line.update(ctx)
 
+    if(gameStatus === "gameOver")
+        targetPoint.draw(ctx)
+
     rabbit.update(ctx)
 
+    tunnel.draw(ctx)
 
-    trap.draw(ctx)
     tunnelEntrance.draw(ctx)
     tunnelExit.draw(ctx)
 
+    if(gameStatus !== "gameOver")
+        targetPoint.draw(ctx)
+
     if(gameStatus === "gameOver") {
+        trap.draw(ctx)
+        ctx.fillStyle = textColor
         ctx.font = "30px Comic"
         ctx.fillText(gameOverText, w/2 -110, h/2 + 60)
     }
